0289f922fc046cb9dcf70ecde1efe8d1b5fdaed7
Vitor <Vitor@DESKTOP-IBVV4TJ>
2021-02-28 19:40:11 -0300

    Processos_ControledeRede

--- src/controller/RedesController.java
+++ src/controller/RedesController.java
@@ -5,74 +5,91 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import javax.swing.JOptionPane;
+
 public class RedesController {
 		public RedesController() {
 		super();
 		}
 		
+		
+		
+		
 		public void ip(String os) {
-			String adapt = " ";
-		 if (os.contains("Windows")) { //validando S.O para Windows
-			 try {
-		Process p = Runtime.getRuntime().exec("ipconfig"); //comando para configuracao ip em windows
-		InputStream fluxo = p.getInputStream(); // trazendo os dados do processo "ipconfig"
-		InputStreamReader leitor = new InputStreamReader(fluxo); //efetuando leitura dos dados
-		BufferedReader buffer = new BufferedReader(leitor); //
-	    String linha = buffer.readLine();
-		    while (linha != null) {
-			    if (linha.contains("Adaptador")) { // condicao para exibir adptador
-				adapt = linha;
-				linha = buffer.readLine();
-			    }
-					if (linha.contains("IPv4")) { // condicao para exibir IPv4
-					String vetIp[] = linha.split(": ");
-					System.out.println(adapt);
-					System.out.println("Endereco do IPv4: " +vetIp[1]);
+		String adapt = " ";
+		if (os.contains("Windows")) { //validando S.O para Windows
+			try {
+			Process p = Runtime.getRuntime().exec("ipconfig"); //comando para configuracao ip em windows
+			InputStream fluxo = p.getInputStream(); // trazendo os dados do processo "ipconfig"
+			InputStreamReader leitor = new InputStreamReader(fluxo); //efetuando leitura dos dados
+			BufferedReader buffer = new BufferedReader(leitor); //
+			String linha = buffer.readLine();
+			StringBuffer tela = new StringBuffer();
+				while (linha != null) {
+					if (linha.contains("Adaptador")) { // condicao para exibir adptador
+					adapt = linha;
 					linha = buffer.readLine();
-			    } 
-					else {
-					linha = buffer.readLine();
-					} //do else
+					}
+						if (linha.contains("IPv4")) { // condicao para exibir IPv4
+						String vetIp[] = linha.split(": ");
+						tela.append(adapt);
+						tela.append("\n");
+						tela.append("Endereco do IPv4: " +vetIp[1]);
+						tela.append("\n\n");
+						linha = buffer.readLine();
+						} 
+						else {
+						linha = buffer.readLine();
+						} 
 				} 
-		buffer.close();
-		leitor.close();
-		fluxo.close();
-		} catch (IOException e) { //catch - saida do try
+			JOptionPane.showMessageDialog(null, tela);
+			buffer.close();
+			leitor.close();
+			fluxo.close();
+			} catch (IOException e) { 
 			e.printStackTrace();
-	}// fim do catch
-			 
-} // do if (S.O)
+			}
+		} // do if (S.O)
 		 else { // do s.o
-			 try {
-			 Process p = Runtime.getRuntime().exec("ifconfig"); //comando para configuracao ip em Linux
-			 InputStream fluxo = p.getInputStream(); // trazendo os dados do processo "ifconfig"
-			 InputStreamReader leitor = new InputStreamReader(fluxo); //efetuando leitura dos dados
-			 BufferedReader buffer = new BufferedReader(leitor); //
-			 String linha = buffer.readLine();
-			  while (linha != null) {
-				if (linha.contains("mtu")) { // condicao para exibir adptador
-				  adapt = linha;
-				  linha = buffer.readLine();
-				  if (linha.contains("inet")) {
-				  String inet = linha;
-				  String[] vetorInet = inet.split(" "); // criando split com a variavel inet (linha do inet linux)
-				 System.out.println("Adaptador: " + adapt);
-				 System.out.println("Ipv4 [inet]: " + vetorInet[9]); // imprimindo "decima" posicaoo do vetor (onde fica localizado o inet)"
-				  } // do if contains inet
-				  } // do if mtu.
-			 else { //else do adptador
-					linha = buffer.readLine();
-				} // do else
-			} // do while
-					buffer.close();
-					leitor.close();
-					fluxo.close();
-					} catch (IOException e) { //catch - saida do try
+			try {
+			Process p = Runtime.getRuntime().exec("ifconfig"); //comando para configuracao ip em Linux
+			InputStream fluxo = p.getInputStream(); // trazendo os dados do processo "ifconfig"
+			InputStreamReader leitor = new InputStreamReader(fluxo); //efetuando leitura dos dados
+			BufferedReader buffer = new BufferedReader(leitor); //
+			String linha = buffer.readLine();
+			StringBuffer tela = new StringBuffer();
+				while (linha != null) {
+				 	if (linha.contains("mtu")) { 
+				 	adapt = linha;
+				 	linha = buffer.readLine();
+				 	}
+				 		if (linha.contains("inet")) {
+				 		String inet = linha;
+				 		String[] vetorInet = inet.split(" "); // criando split com a variavel inet (linha do inet linux)
+				 		tela.append("Adaptador: "+adapt);
+				 		tela.append("\n");
+				 		tela.append("Endereco do IPv4 (iNet): " +vetorInet[9]);
+				 		tela.append("\n\n");
+				 	} 
+
+				 		else { 
+				 		linha = buffer.readLine();
+				 		} 
+				} 
+			JOptionPane.showMessageDialog(null, tela);
+			buffer.close();
+			leitor.close();
+			fluxo.close();
+			} catch (IOException e) { //catch - saida do try
 					e.printStackTrace();
-				}// fim do catch
-		 } // do else (s.o)
+			}// fim do catch
+		} // do else (s.o)
 	}//do metodo ip
 						
+		
+		
+		
+		
 	public void ping(String os) {
 	if (os.contains("Windows")) {
 		try {
@@ -81,20 +98,19 @@ public class RedesController {
 			InputStreamReader leitor = new InputStreamReader(fluxo);
 			BufferedReader buffer = new BufferedReader(leitor);
 			String linha = buffer.readLine();
-			while (linha != null) {
-				if (linha.contains("M")) {
+				while (linha != null) {
+					if (linha.contains("M")) {
 					String vetorMedia[] = linha.split("=");
-					System.out.println("Tempo medio de:"+vetorMedia[3]);
-				}
-				linha = buffer.readLine();
-			}
+					JOptionPane.showMessageDialog(null, "Tempo medio de:"+vetorMedia[3]);
+					}
+					linha = buffer.readLine();
+					}
 			buffer.close();
 			leitor.close();
 			fluxo.close();
-					} catch (IOException e) {
-			e.printStackTrace();
-		}
-				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 	} // do if s.o
 	else { 
 		try {
@@ -103,20 +119,20 @@ public class RedesController {
 			InputStreamReader leitor = new InputStreamReader(fluxo);
 			BufferedReader buffer = new BufferedReader(leitor);
 			String linha = buffer.readLine();
-			while (linha != null) {
-				if (linha.contains("mdev")) {
+				while (linha != null) {
+					if (linha.contains("mdev")) {
 					String vetorMedia[] = linha.split("/");
-					System.out.println("Tempo medio do ping: " + vetorMedia[4] + " ms.");
-				}
-				linha = buffer.readLine();
-			}
+					JOptionPane.showMessageDialog(null,"Tempo medio do ping: " + vetorMedia[4] + " ms.");
+					}
+					linha = buffer.readLine();
+					}
 			buffer.close();
 			leitor.close();
 			fluxo.close();
-					} catch (IOException e) {
-			e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-	}
 	} //do metodo ping
 } //da classe
 			
--- src/view/Main.java
+++ src/view/Main.java
@@ -11,7 +11,7 @@ public class Main {
 		String os = System.getProperty("os.name");
 		int option = 1;
 			while (option != 0) {
-			option = Integer.parseInt(JOptionPane.showInputDialog("Sistema operacional: " + os + "\nDigite a opcao desejada\n1- Verificar adaptadores e seus respectivos IPs.\n2- Verificar o tempo medio do seu ping\n0- Encerrar a aplicacao"+"\n\nOS RESULTADOS APARECERAO NO CONSOLE DA IDE"));
+			option = Integer.parseInt(JOptionPane.showInputDialog("Sistema operacional: " + os + "\n\nDigite a opcao desejada:\n1- Verificar adaptadores e seus respectivos IPs.\n2- Verificar o tempo medio do seu ping (aguardar alguns segundos)\n0- Encerrar a aplicacao"+"\n\n"));
 		switch (option) {
 		case 1:
 			redes.ip(os);
